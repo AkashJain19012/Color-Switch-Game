6bbcacccf3456ecb6b911f28db75a541289a8d8e
AkashJain19012 <63505487+AkashJain19012@users.noreply.github.com>
2020-11-21 01:39:27 +0530

    Ball class template
    Ball class is extended from circle class. Similarly we need to do for other basic classes.

---
+++ Ball.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+
+public class Ball extends Circle {
+	
+	
+	Ball(double centerX, double centerY, double radius, Color color)
+	{
+		super(centerX, centerY, radius, color);
+	}
+	
+	public void moveUp()
+	{
+		
+	}
+	
+	public void moveDown()
+	{
+		
+	}
+	
+	public void setColor(Color c)
+	{
+		
+	}
+	
+
+}
