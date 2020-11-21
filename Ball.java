58e3181d5ddde284cb153664ea4dbf4eb425c128
Pranansh <58323002+Pranansh@users.noreply.github.com>
2020-11-21 12:35:50 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,10 +1,10 @@
 package application;
 
 import javafx.scene.shape.Circle;
+import javafx.animation.AnimationTimer;
 import javafx.scene.paint.Color;
 
 public class Ball extends Circle {
-	
 	
 	Ball(double centerX, double centerY, double radius, Color color)
 	{
@@ -13,17 +13,37 @@ public class Ball extends Circle {
 	
 	public void moveUp()
 	{
-		
+		setY(-6);	
 	}
 	
 	public void moveDown()
 	{
-		
+		setY(3);
+	}
+	
+	public double getX() 
+	{
+		return this.getCenterX();
+	}
+	
+	public double getY() 
+	{
+		return this.getCenterY();
+	}
+	
+	public void setX(double dist) 
+	{
+		this.setCenterX(getX()+dist);
+	}
+	
+	public void setY(double dist) 
+	{
+		this.setCenterY(getY()+dist);
 	}
 	
 	public void setColor(Color c)
 	{
-		
+		this.setFill(c);
 	}
 	
 
