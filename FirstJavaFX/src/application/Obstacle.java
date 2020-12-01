862e711cc7d02000fce74560df2fff12af54c738
AkashJain19012 <63505487+AkashJain19012@users.noreply.github.com>
2020-12-01 23:32:33 +0530

    Update Obstacle.java

--- FirstJavaFX/src/application/Obstacle.java
+++ FirstJavaFX/src/application/Obstacle.java
@@ -1,25 +1,49 @@
 package application;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 
-public class Obstacle {
+public abstract class Obstacle {
 	
 	String type;
+	Group root;
+	
 	
 	Obstacle(String type){
 		this.type=type;
 	}	
 	
-	public void moveUp(Group root)
+	public void moveUp()
 	{
-		root.setLayoutY(root.getLayoutY()-1);
+		root.setLayoutY(root.getLayoutY()-3);
 	}
 	
-	public void moveDown(Group root)
+	public void moveDown()
 	{
-		root.setLayoutY(root.getLayoutY()+1);
+		root.setLayoutY(root.getLayoutY()+3);
 	}
+	
+	public void rotate() 
+	{
+		RotateTransition rotate = new RotateTransition();
+        rotate.setAxis(Rotate.Z_AXIS);
+        rotate.setByAngle(360);
+        rotate.setRate(0.1);
+        rotate.setCycleCount(Timeline.INDEFINITE);
+        rotate.setDuration(Duration.INDEFINITE); 
+        rotate.setNode(root);
+        rotate.play();
+	}
+	
+	public abstract int[] checkCollision(Ball player,int outside,int upper);
+	
+	public abstract Group create(int y);
+	
+	public abstract Group getRoot();
 	
 	
 }
